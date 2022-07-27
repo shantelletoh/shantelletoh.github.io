c0c091373cec59f9f2825fd37dbf7a0f8998dcf2
shantelletoh <shantelleschool@gmail.com>
2022-07-26 22:54:00 -0700

    fix footer noreferrer warning

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -9,18 +9,30 @@ export default function Footer() {
     <footer class="mt-10 py-8 bg-neutral-200 text-center">
       <div>
         <div class="mb-5 flex justify-center gap-4">
-          <a target="_blank" href="mailto:shantelletoh@gmail.com">
+          <a
+            target="_blank"
+            href="mailto:shantelletoh@gmail.com"
+            rel="noreferrer"
+          >
             <div className="rounded-full w-10 h-10 border border-neutral-500 hover:border-neutral-800 text-neutral-500 hover:text-neutral-800 flex justify-center items-center">
               <HiOutlineMail className="w-7 h-7" />
             </div>
           </a>
 
-          <a target="_blank" href="https://github.com/shantelletoh">
+          <a
+            target="_blank"
+            href="https://github.com/shantelletoh"
+            rel="noreferrer"
+          >
             <div className="rounded-full w-10 h-10 border border-neutral-500 hover:border-neutral-800 text-neutral-500 hover:text-neutral-800 flex justify-center items-center">
               <FaGithub className="w-7 h-7" />
             </div>
           </a>
-          <a target="_blank" href="https://linkedin.com/in/shantelletoh">
+          <a
+            target="_blank"
+            href="https://linkedin.com/in/shantelletoh"
+            rel="noreferrer"
+          >
             <div className="rounded-full w-10 h-10 border border-neutral-500 hover:border-neutral-800 text-neutral-500 hover:text-neutral-800 flex justify-center items-center">
               <FaLinkedin className="w-6 h-6" />
             </div>
